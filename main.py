fc0f822534a4ad94eaf5df6a7afadf43964b8fdd
Lifanov Fedor <amagooseoj@mail.ru>
2023-03-28 20:46:19 +0500

    init

---
+++ main.py
@@ -0,0 +1,158 @@
+"""# Check nvcc version
+!nvcc -V
+# Check GCC version
+!gcc --version
+# Install PyTorch
+!pip install torch==1.12.0 torchvision --extra-index-url https://download.pytorch.org/whl/cu113
+# Install MMCV
+!pip install openmim
+
+!mim install mmcv-full==1.6.0
+!rm -rf mmsegmentation
+!git clone https://github.com/open-mmlab/mmsegmentation.git
+%cd mmsegmentation
+!pip install -e .
+
+!mkdir checkpoints
+!wget https://download.openmmlab.com/mmsegmentation/v0.5/pspnet/pspnet_r50-d8_512x1024_40k_cityscapes/pspnet_r50-d8_512x1024_40k_cityscapes_20200605_003338-2966598c.pth -P checkpoints
+"""
+import torch, torchvision
+import sys
+import mmcv
+import os.path as osp
+from mmseg.datasets.builder import DATASETS
+from mmseg.datasets.custom import CustomDataset
+from mmcv import Config
+from mmseg.apis import set_random_seed
+from mmseg.utils import get_device
+from mmseg.datasets import build_dataset
+from mmseg.models import build_segmentor
+from mmseg.apis import train_segmentor
+
+PATH_TO_SAVE = ''
+LOG_FILE = ''
+data_root = '../train'
+img_dir = ''
+ann_dir = ''
+
+classes = ('road', 'notroad')
+palette = [[255, 255, 255], [0, 0, 0]]
+
+split_dir = 'splits'
+mmcv.mkdir_or_exist(osp.join(data_root, split_dir))
+filename_list = [osp.splitext(filename)[0] for filename in mmcv.scandir(
+   data_root, suffix='.png')]
+
+with open(osp.join(data_root, split_dir, 'train.txt'), 'w') as f:
+  train_length = int(len(filename_list)*4/5)
+  f.writelines(line + '\n' for line in filename_list[:train_length])
+with open(osp.join(data_root, split_dir, 'val.txt'), 'w') as f:
+  f.writelines(line + '\n' for line in filename_list[train_length:])
+
+@DATASETS.register_module()
+class StanfordBackgroundDatasett(CustomDataset):
+  CLASSES = classes
+  PALETTE = palette
+  def __init__(self, split, **kwargs):
+    super().__init__(img_suffix='.jpg', seg_map_suffix='.png',
+                     split=split, **kwargs)
+    assert osp.exists(self.img_dir) and self.split is not None
+
+cfg = Config.fromfile('configs/pspnet/pspnet_r50-d8_512x1024_40k_cityscapes.py')
+cfg.norm_cfg = dict(type='BN', requires_grad=True)
+cfg.model.backbone.norm_cfg = cfg.norm_cfg
+cfg.model.decode_head.norm_cfg = cfg.norm_cfg
+cfg.model.auxiliary_head.norm_cfg = cfg.norm_cfg
+cfg.model.decode_head.num_classes = 2
+cfg.model.auxiliary_head.num_classes = 2
+
+cfg.dataset_type = 'StanfordBackgroundDatasett'
+cfg.data_root = data_root
+
+cfg.data.samples_per_gpu = 8
+cfg.data.workers_per_gpu=8
+
+cfg.img_norm_cfg = dict(
+    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+cfg.crop_size = (256, 256)
+cfg.train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='LoadAnnotations'),
+    dict(type='Resize', img_scale=(1024, 1024), ratio_range=(0.5, 2.0)),
+    dict(type='RandomCrop', crop_size=cfg.crop_size, cat_max_ratio=0.75),
+    dict(type='RandomFlip', flip_ratio=0.5),
+    dict(type='PhotoMetricDistortion'),
+    dict(type='Normalize', **cfg.img_norm_cfg),
+    dict(type='Pad', size=cfg.crop_size, pad_val=0, seg_pad_val=255),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'gt_semantic_seg']),
+]
+
+cfg.test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(
+        type='MultiScaleFlipAug',
+        img_scale=(1024, 1024),
+        flip=False,
+        transforms=[
+            dict(type='Resize', keep_ratio=True),
+            dict(type='RandomFlip'),
+            dict(type='Normalize', **cfg.img_norm_cfg),
+            dict(type='ImageToTensor', keys=['img']),
+            dict(type='Collect', keys=['img']),
+        ])
+]
+
+cfg.data.train.type = cfg.dataset_type
+cfg.data.train.data_root = cfg.data_root
+cfg.data.train.img_dir = img_dir
+cfg.data.train.ann_dir = ann_dir
+cfg.data.train.pipeline = cfg.train_pipeline
+cfg.data.train.split = 'splits/train.txt'
+
+cfg.data.val.type = cfg.dataset_type
+cfg.data.val.data_root = cfg.data_root
+cfg.data.val.img_dir = img_dir
+cfg.data.val.ann_dir = ann_dir
+cfg.data.val.pipeline = cfg.test_pipeline
+cfg.data.val.split = 'splits/val.txt'
+
+cfg.data.test.type = cfg.dataset_type
+cfg.data.test.data_root = cfg.data_root
+cfg.data.test.img_dir = img_dir
+cfg.data.test.ann_dir = ann_dir
+cfg.data.test.pipeline = cfg.test_pipeline
+cfg.data.test.split = 'splits/val.txt'
+
+
+cfg.load_from = 'checkpoints/pspnet_r50-d8_512x1024_40k_cityscapes_20200605_003338-2966598c.pth'
+
+cfg.work_dir = './work_dirs/tutorial'
+
+cfg.runner.max_iters = 200
+cfg.log_config.interval = 10
+cfg.evaluation.interval = 200
+cfg.checkpoint_config.interval = 200
+
+
+cfg.seed = 0
+set_random_seed(0, deterministic=False)
+cfg.gpu_ids = range(1)
+cfg.device = get_device()
+
+# Build the dataset
+datasets = [build_dataset(cfg.data.train)]
+
+# Build the detector
+model = build_segmentor(cfg.model)
+# Add an attribute for visualization convenience
+model.CLASSES = datasets[0].CLASSES
+
+# Create work_dir
+mmcv.mkdir_or_exist(osp.abspath(cfg.work_dir))
+
+with open(LOG_FILE, 'w') as sys.stdout:
+    train_segmentor(model, datasets, cfg, distributed=False, validate=True,
+        meta=dict())
+
+torch.save(model, PATH_TO_SAVE)
